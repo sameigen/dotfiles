a7b4e424fd5fc53b030578847408f045b8eccec4
Sam Eigen <seigen22@gmail.com>
2021-10-29 15:47:13 -0500

    testing

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
+starship init fish | source
+
+export TERM=xterm-256color
+
 set -g -x PATH /usr/local/bin $PATH
 set -g -x fish_greeting ''
 
-alias vim='~/Downloads/nvim-osx64/bin/nvim'
-alias nvim='vim'
-alias vrc='~/Downloads/nvim-osx64/bin/nvim ~/Desktop/Personal/dotfiles/nvim/init.vim'
+alias vim='nvim'
+alias vrc='nvim ~/dotfiles/nvim/init.vim'
