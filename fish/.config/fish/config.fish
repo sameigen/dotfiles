e64d934fe0faaa0fc7fc5c6431cc2e30eb766c3f
Sam Eigen <se2514@columbia.edu>
2021-10-31 01:40:20 -0500

    done

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,6 @@ set -g -x fish_greeting ''
 alias vim='~/Downloads/nvim-osx64/bin/nvim'
 alias nvim='vim'
 alias vrc='~/Downloads/nvim-osx64/bin/nvim ~/Desktop/Personal/dotfiles/nvim/init.vim'
-alias pw='cat ~/Desktop/Personal/dotfiles/mst'
+alias pw='cat ~/dotfiles/mst | pbcopy'
 
 starship init fish | source
